7730edfecbbdbe362dde26bada5e06198ea26ab5
kanadkapil <118840231+kanadkapil@users.noreply.github.com>
2024-06-03 23:43:10 +0530

    Add files via upload

---
+++ Constructor.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class A{
+	public:
+		A(){
+			cout<<"CSE 202";
+		}
+		void print(){
+			cout<<"Anything";
+		}
+	
+};
+
+int main(){
+	A obj;
+	
+	return 0;
+}
