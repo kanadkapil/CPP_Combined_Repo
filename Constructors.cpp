75cc00db6559c6eaa4b90149f64595e131b14396
kanadkapil <118840231+kanadkapil@users.noreply.github.com>
2024-06-04 10:53:27 +0530

    Add files via upload

---
+++ Constructors.cpp
@@ -0,0 +1,35 @@
+/*#include <iostream>
+using namespace std;
+
+class A{
+	int length, width;
+	
+	public:
+//		A(){
+//			cout<<"Hello";
+//		}
+//		//Copy constructor
+//		A(const A& ob){
+//			cout<<" World";
+//		}
+//Constructor with parameters
+	A (int a, int b){
+			length=a;
+			width=b;
+			int area=length*width;
+			cout<<"\n The area is: "<<area;
+		}
+};
+
+int main()
+{
+//	A ob;
+//	A ob2(ob);
+	A ob(4,9);
+	return 0;
+}
+*/
+
+
+
+
