68f516d7ad1255b1a799e7736199d6a3181f2d6c
kanadkapil <118840231+kanadkapil@users.noreply.github.com>
2024-06-06 21:57:32 +0530

    Add files via upload

---
+++ square of number.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    double x;
+    cin>>x;
+    double sqrt_x = sqrt(x);
+    cout<<"The Square value of x is "<<sqrt_x<<endl;
+    
+    return 0;
+}
