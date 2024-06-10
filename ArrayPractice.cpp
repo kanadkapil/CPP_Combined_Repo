6a94e7414dcb525a6c14655c843d9b64e37a6d33
kanadkapil <118840231+kanadkapil@users.noreply.github.com>
2024-06-11 00:18:15 +0530

    Add files via upload

---
+++ ArrayPractice.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    int a[5]={1,2,3,4,5};
+    cout<<a[3]<<endl;
+    for (int i = 0 ; i <= 5; i++){
+        cout<<a[2]<<endl;
+    }
+    return 0;
+}
---
+++ array age.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    short age[4];
+    age[0]=32;
+    age[1]=56;
+    age[2]=78;
+    age[3]=43;
+    cout<<age[0]<<" ";
+    cout<<age[1]<<" ";
+    cout<<age[2]<<" ";
+    cout<<age[3]<<" ";
+    return 0;
+}
